e91172b68ffd9ae45dc2d55390c137d9bc632faa
Erza Bërbatovci <116487812+erza-b@users.noreply.github.com>
2024-04-11 22:18:08 +0200

    Implement user signup endpoint with JWT authentication

--- src/main/java/com/foodwebsite/controller/AuthController.java
+++ src/main/java/com/foodwebsite/controller/AuthController.java
@@ -1,8 +1,22 @@
 package com.foodwebsite.controller;
 
 
+import com.foodwebsite.config.JwtProvider;
+import com.foodwebsite.model.Cart;
+import com.foodwebsite.model.User;
+import com.foodwebsite.repository.CartRepository;
 import com.foodwebsite.repository.UserRepository;
+import com.foodwebsite.response.AuthResponse;
+import com.foodwebsite.service.CustomerUserDetailsService;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +25,49 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class AuthController {
 
+    @Autowired
     private UserRepository userRepository;
+    @Autowired
     private PasswordEncoder passwordEncoder;
-    private
+    @Autowired
+    private JwtProvider jwtProvider;
+    @Autowired
+    private CustomerUserDetailsService customerUserDetailsService;
+    @Autowired
+    private CartRepository cartRepository;
+
+    @PostMapping("/signup")
+
+    public ResponseEntity<AuthResponse>createUserHandler(@RequestBody User user)throws Exception{
+
+        User isEmailExist=userRepository.findByEmail(user.getEmail());
+        if(isEmailExist!=null){
+            throw new Exception("Email is already used with another account");
+        }
+
+        User createdUser=new User();
+        createdUser.setEmail(user.getEmail());
+        createdUser.setFullName(user.getFullName());
+        createdUser.setRole(user.getRole());
+        createdUser.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        User savedUser=userRepository.save(createdUser);
+
+        Cart cart=new Cart();
+        cart.setCustomer(savedUser);
+        cartRepository.save(cart);
+
+        Authentication authentication=new UsernamePasswordAuthenticationToken(user.getEmail(),user.getPassword());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt=jwtProvider.generateToken(authentication);
+        AuthResponse authResponse=new AuthResponse();
+        authResponse.setJwt(jwt);
+        authResponse.setMessage("Register success");
+        authResponse.setRole(savedUser.getRole());
+
+        return new ResponseEntity<>(authResponse, HttpStatus.CREATED);
+
+        return null;
+    }
 }
---
+++ src/main/java/com/foodwebsite/response/AuthResponse.java
@@ -0,0 +1,12 @@
+package com.foodwebsite.response;
+
+import com.foodwebsite.model.USER_ROLE;
+import lombok.Data;
+
+@Data
+
+public class AuthResponse {
+    private String jwt;
+    private String message;
+    private USER_ROLE role;
+}
