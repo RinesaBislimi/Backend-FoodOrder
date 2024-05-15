d9a13bbc9629210494950deb68bc570e77cea2bf
fjolla-ajeti <fjollaajeti9@gmail.com>
2024-05-15 11:36:46 +0200

    Implement PaymentService to create payment link using Stripe API

---
+++ src/main/java/com/foodwebsite/service/PaymentService.java
@@ -0,0 +1,11 @@
+package com.foodwebsite.service;
+
+import com.foodwebsite.model.Order;
+import com.foodwebsite.response.PaymentResponse;
+import com.stripe.exception.StripeException;
+
+
+
+public interface PaymentService {
+    public PaymentResponse createPaymentLink(Order order) throws StripeException;
+}
