7494b1257a8ef4e7aeda9642f008419b8f30c0ae
Rinesa Bislimi <118773246+RinesaBislimi@users.noreply.github.com>
2024-05-18 03:55:55 +0200

    Add Ingredients endpoints

--- src/main/java/com/foodwebsite/controller/CategoryController.java
+++ src/main/java/com/foodwebsite/controller/CategoryController.java
@@ -6,12 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.lang.String;
 
 import com.foodwebsite.model.Category;
 import com.foodwebsite.model.User;
 import com.foodwebsite.service.CategoryService;
 import com.foodwebsite.service.UserService;
-import com.mysql.cj.x.protobuf.MysqlxDatatypes.Scalar.String;
 
 @RestController
 @RequestMapping("/api")
@@ -23,22 +23,29 @@ public class CategoryController {
     @Autowired
     private UserService userService;
 
+
     @PostMapping("/admin/category")
     public ResponseEntity<Category> createCategory(@RequestBody Category category,
                                                    @RequestHeader("Authorization") String jwt) throws Exception {
 
-        User user = userService.findUserByJwtToken(null);
-        Category createCategory = categoryService.createCategory(category.getName(), user.getId());                                      
+        User user = userService.findUserByJwtToken(jwt); // Pass the jwt token here
+        Category createCategory = categoryService.createCategory(category.getName(), user.getId());
         return new ResponseEntity<>(createCategory, HttpStatus.CREATED);
     }
+
 
     @GetMapping("/category/restaurant/{id}")
     public ResponseEntity<List<Category>> getRestaurantCategory(
             @PathVariable Long id,
-            @RequestHeader("Authorization") String jwt) throws Exception {
+            @RequestHeader(value = "Authorization", required = false) String jwt) throws Exception {
 
-        User user = userService.findUserByJwtToken(null);
-        List <Category> categories = categoryService.findCategoryByRestaurantId(id);
-        return new ResponseEntity<>(categories, HttpStatus.CREATED);
+        User user = null;
+        if (jwt != null) {
+            user = userService.findUserByJwtToken(jwt);
+        }
+
+        List<Category> categories = categoryService.findCategoryByRestaurantId(id);
+        return new ResponseEntity<>(categories, HttpStatus.OK);
     }
+
 }
